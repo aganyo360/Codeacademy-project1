e9ad22f67421812eda8efdefd9a31ccbc4a7f2c6
aganyo360 <wilsonaganyo@gmail.com>
2024-07-04 00:06:38 +0300

    updated views

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
 def index(request):
-    return HttpResponse("hello Kenya")
+    movies = Movie.objects.all()
+    output = ', '.join([m.title for m in movies])
+    return HttpResponse(output)
