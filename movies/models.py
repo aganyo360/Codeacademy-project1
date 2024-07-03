24b807ae2b568cbc21d629dda8f8dce3f38e76d9
aganyo360 <wilsonaganyo@gmail.com>
2024-07-03 23:15:23 +0300

    added models to models file

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    release_date = models.DateField()
+    number_in_stock = models.IntegerField()
+    daily_rate = models.FloatField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
