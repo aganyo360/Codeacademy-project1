823151e81df012f0fe83552248c025143d543f73
aganyo360 <wilsonaganyo@gmail.com>
2024-07-03 23:55:54 +0300

    updted models

--- movies/models.py
+++ movies/models.py
@@ -5,6 +5,10 @@ from django.utils import timezone
 class Genre(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     release_date = models.DateField()
@@ -12,3 +16,6 @@ class Movie(models.Model):
     daily_rate = models.FloatField()
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     date_created = models.DateTimeField(default= timezone.now)
+
+    def __str__(self):
+        return self.title
