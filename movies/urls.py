407c9220bc1f0311f7f680e4a6f816cdd63310d3
aganyo360 <wilsonaganyo@gmail.com>
2024-07-04 10:13:53 +0300

    added movie_detail and genre urls

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns= [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('movie_detail', views.movie_detail, name='movie_detail'),
+    path('genre_detail', views.genre_detail, name ='genre_detail')
 ]
