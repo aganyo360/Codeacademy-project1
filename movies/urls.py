42f2098028c1c7927d62aac14dc1aba79b7ef40a
aganyo360 <wilsonaganyo@gmail.com>
2024-07-04 12:16:04 +0300

    added app_name

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
+app_name = 'movies'
 urlpatterns= [
     path('', views.index, name='movies_index'),
-    path('<int:movie_id>', views.detail, name='movies_detail'),
-    path('movie_detail', views.movie_detail, name='movie_detail'),
+    path('<int:movie_id>', views.detail, name='detail'),
     path('genre_detail', views.genre_detail, name ='genre_detail')
 ]
