4edd3d89b17d87ec392c217ceffcf34cec73d3ba
aganyo360 <wilsonaganyo@gmail.com>
2024-07-04 12:15:48 +0300

    handled errors with get_object_or_404

--- movies/views.py
+++ movies/views.py
@@ -1,17 +1,13 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Movie, Genre
 # Create your views here.
 def index(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies':movies})
 def detail(request, movie_id):
-    try:
-        movie = Movie.objects.get(id=movie_id)
-        return render(request, 'movies/detail.html', {'movie':movie})
-    except Movie.DoesNotExist:
-        raise Http404("Movie not found")
-
+   movie= get_object_or_404(Movie, pk=movie_id)
+   return render(request,'movies/detail.html', {'movie': movie})
 
 
 
