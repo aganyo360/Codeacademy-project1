80e14c086aa884cf1bb53c526ea5cc5348f6383c
aganyo360 <wilsonaganyo@gmail.com>
2024-07-04 10:14:12 +0300

    added movie_detail and genre views

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Movie
+from .models import Movie, Genre
 # Create your views here.
 def index(request):
     movies = Movie.objects.all()
     output = ', '.join([m.title for m in movies])
+    return HttpResponse(output)
+
+def movie_detail(request):
+    movies = Movie.objects.get(id=2)
+    # context = 
+    return HttpResponse(movies)
+
+def genre_detail(request):
+    genres = (Genre.objects.all())
+    output = '    \n' .join([g.name for g in genres])
     return HttpResponse(output)
